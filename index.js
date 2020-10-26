dcdb058f910a117b1308f458bcf340d36b5428db
good-ho <whdxo100@gmail.com>
2020-10-26 22:27:11 +0900

    babel node & nodemon
    babel node & nodemon
    1. babel node install and adjust
    2. nodemon install and adjust

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express = require('express')
+import express from "express";
+
+// const express = require('express')
 const app = express()
 const port = 3000
 
+
+const handleProfile = (req, res) =>{
+    res.send("This is profile");
+}
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World !!!')
 })
+
+app.get("/profile", handleProfile);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
