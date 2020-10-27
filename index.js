fa7544c39fdb17e00809cedf3963da988bce71a3
good-ho <whdxo100@gmail.com>
2020-10-27 18:00:30 +0900

    Express Core Middlewares
    1. middleware 정의
    2. morgan install
    3. helmet install
    4. body-parse install
    5. cookie-parse install

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
-// const express = require('express')
 const app = express()
 const port = 3000
-
 
 const handleProfile = (req, res) =>{
     res.send("This is profile");
 }
 
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(morgan("dev"));
+app.use(helmet());
+
 app.get('/', (req, res) => {
-  res.send('Hello World !!!')
+  res.send('Hello World !!!');
 })
 
 app.get("/profile", handleProfile);
