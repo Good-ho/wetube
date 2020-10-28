4b1c0df6bd866e832f9c5ae5106b0efb924ef7d8
good-ho <whdxo100@gmail.com>
2020-10-28 22:02:47 +0900

    Search Controller
     controller에서 query 정보 확인

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,5 +1,10 @@
 export const home = (req, res) => res.render("home", {pageTitle:"Home"});
-export const search = (req, res) => res.render("search", {pageTitle:"Search"});
+export const search = (req, res) => {
+    // console.log(req.query.term);
+    // const searchingBy = req.query.term;
+    const {query:{term : searchingBy}} = req;
+    res.render("search", {pageTitle:"Search", searchingBy})
+};
 export const upload = (req, res) => res.render("upload", {pageTitle:"Uplaod"});
 export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle:"VideoDetail"});
 export const editVideo = (req, res) => res.render("editVideo", {pageTitle:"editVideo"});
