dc005c1e9ec513aca7ac24a8ef9fa0c92cef9cfd
good-ho <whdxo100@gmail.com>
2020-11-01 21:12:27 +0900

    1. Connecting to MongoDB 2. dotenv install

--- db.js
+++ db.js
@@ -1,41 +1,25 @@
-export const videos = [
-    {
-        id:3241,
-        title:"Video awesome",
-        description: "This is something dummy one",
-        views:24,
-        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator:{
-            id:1212,
-            name:"ho",
-            email:"ho@gmail.com"            
-        }
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-    },
-    {
-        id:1212,
-        title:"Video nice",
-        description: "This is something dummy two",
-        views:24,
-        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator:{
-            id:1212,
-            name:"ho",
-            email:"ho@gmail.com"            
-        }
+dotenv.config();
 
-    },
+mongoose.connect(process.env.MONGO_URL, 
     {
-        id:2323,
-        title:"Video perfect",
-        description: "This is something dummy three",
-        views:24,
-        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator:{
-            id:1212,
-            name:"ho",
-            email:"ho@gmail.com"            
-        }
+        useNewUrlParser:true,
+        useFindAndModify:false
+    }
+);
 
-    }         
-]
+const db = mongoose.connection;
+
+const handleOpen = () => {
+    console.log("Connect to DB");
+}
+
+const handleError = (error) => {
+    console.log(`db error : ${error}`);
+}
+
+db.once("open", handleOpen);
+db.on("error", handleError);
+
