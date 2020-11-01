0e2fd0677333e25afa135c6e7a690e0e9d7e04cc
good-ho <whdxo100@gmail.com>
2020-11-01 16:56:07 +0900

    Join Controller

--- controller/videoController.js
+++ controller/videoController.js
@@ -8,7 +8,7 @@ export const search = (req, res) => {
     // console.log(req.query.term);
     // const searchingBy = req.query.term;
     const {query:{term : searchingBy}} = req;
-    res.render("search", {pageTitle:"Search", searchingBy})
+    res.render("search", {pageTitle:"Search", searchingBy, videos})
 };
 export const upload = (req, res) => res.render("upload", {pageTitle:"Uplaod"});
 export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle:"VideoDetail"});
