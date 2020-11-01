8857f6e15b944e10a006aa82c15024eb5821d6e2
good-ho <whdxo100@gmail.com>
2020-11-01 12:53:01 +0900

    Home Controller  Part one

---
+++ db.js
@@ -0,0 +1,41 @@
+export const videos = [
+    {
+        id:3241,
+        title:"Video awesome",
+        description: "This is something dummy one",
+        views:24,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator:{
+            id:1212,
+            name:"ho",
+            email:"ho@gmail.com"            
+        }
+
+    },
+    {
+        id:1212,
+        title:"Video nice",
+        description: "This is something dummy two",
+        views:24,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator:{
+            id:1212,
+            name:"ho",
+            email:"ho@gmail.com"            
+        }
+
+    },
+    {
+        id:2323,
+        title:"Video perfect",
+        description: "This is something dummy three",
+        views:24,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator:{
+            id:1212,
+            name:"ho",
+            email:"ho@gmail.com"            
+        }
+
+    }         
+]
