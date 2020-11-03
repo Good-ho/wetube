928f021b9de1e3939173f6fb96b70595383b3ffd
good-ho <whdxo100@gmail.com>
2020-11-03 21:57:00 +0900

    Edit Video get 수정. editVideo post 추가.

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,14 +1,22 @@
 import express from "express";
-import { deleteVideo, editVideo, getUpload, postUpload, videoDetail, videos } from "../controller/videoController";
+import { deleteVideo, getEditVideo, getUpload, postEditVideo, postUpload, videoDetail, videos } from "../controller/videoController";
 import { uploadVideo } from "../middlewares";
 import routes from "../routes";
 
 const videoRouter = express.Router();
 
+//Upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
-videoRouter.get(routes.editVideo, editVideo);
+
+// Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+// delete Video
 videoRouter.get(routes.deleteVideo, deleteVideo);
+
+// Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
 
 export default videoRouter;
