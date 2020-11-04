19baec42e95813cb07b60912445e3b5ecef589ed
good-ho <whdxo100@gmail.com>
2020-11-04 20:04:12 +0900

    eslint 설치

--- controller/userController.js
+++ controller/userController.js
@@ -1,40 +1,38 @@
 import routes from "../routes";
 
 export const getJoin = (req, res) => {
-    res.render("join", {pageTitle:"Join"})
+  res.render("join", { pageTitle: "Join" });
 };
 
 export const postJoin = (req, res) => {
-    // console.log(req.body);
-    const {
-        body:{
-            name,
-            email,
-            password,
-            password2
-        }
-    } = req;
+  // console.log(req.body);
+  const {
+    body: { name, email, password, password2 },
+  } = req;
 
-    if(password !== password2){
-        res.status(400);
-        res.render("join", {pageTitle:"Join"})
-    } else {
-        // TO DO : Register User
-        // TO DO : lOG USER IN
-        res.redirect(routes.home);        
-    }    
+  if (password !== password2) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    // TO DO : Register User
+    // TO DO : lOG USER IN
+    res.redirect(routes.home);
+  }
 };
 
 export const getLogin = (req, res) => {
-    res.render("login", {pageTitle:"Login"})
+  res.render("login", { pageTitle: "Login" });
 };
 export const postLogin = (req, res) => {
-    res.redirect(routes.home);
-}
+  res.redirect(routes.home);
+};
 
 export const logout = (req, res) => {
-    res.redirect(routes.home);
-}
-export const userDetail = (req, res) => res.render("userDetail", {pageTitle:"userDetail"});
-export const editProfile = (req, res) => res.render("editProfile", {pageTitle:"editProfile"});
-export const changePassword = (req, res) => res.render("changePassword", {pageTitle:"changePassword"});
+  res.redirect(routes.home);
+};
+export const userDetail = (req, res) =>
+  res.render("userDetail", { pageTitle: "userDetail" });
+export const editProfile = (req, res) =>
+  res.render("editProfile", { pageTitle: "editProfile" });
+export const changePassword = (req, res) =>
+  res.render("changePassword", { pageTitle: "changePassword" });
